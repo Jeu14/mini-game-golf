43c1208ae17f3a40dc8bc8eb143d4f33672ac296
Jeu14 <jedissonsantana34@gmail.com>
2024-05-14 13:14:31 -0300

    feat: Add events on script

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const bolinhaElemento = document.querySelector(".bolinha");
+let posicao = 0;
+
+function ganhou() {
+  setTimeout(() => {
+    alert("Parabéns, você chegou!!");
+    reiniciar();
+  }, 500);
+}
+
+function perdeu() {
+  setTimeout(() => {
+    alert("Poxa, você errou. Tente novamente.");
+    reiniciar();
+  }, 500);
+}
+
+function moverBolinha(event) {
+  if (event.keyCode == 39) {
+    posicao += 10;
+    bolinhaElemento.style.marginLeft = `${posicao}px`;
+    if (posicao == 400) {
+      ganhou();
+    }
+  }
+}
+
+document.addEventListener("keydown", moverBolinha);
+
+function reiniciar() {
+  posicao = 0;
+  bolinhaElemento.style.marginLeft = `${posicao}px`;
+}
+
+function girar() {
+  posicao = Math.random() * 400;
+  bolinhaElemento.style.marginLeft = `${posicao}px`;
+  if (posicao == 400) {
+    ganhou();
+  } else {
+    perdeu();
+  }
+}
